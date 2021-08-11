7f0089a9d64f1c186193950f66c38fede1453a16
Helder Morais <helder.morais@gmail.com>
2021-08-11 16:40:54 -0300

    Zzz

---
+++ my-component-library/vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+    // options...
+    publicPath: '',
+    filenameHashing: false,
+    css: {
+          extract: false,
+         }
+  }
